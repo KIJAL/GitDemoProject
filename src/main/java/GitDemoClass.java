d128b531022abbca983af61cbcebe63751315778
Kijal <coolguyisme12@gmail.com>
2021-06-30 20:34:11 +1200

    Initial commit:testing2

--- src/main/java/GitDemoClass.java
+++ src/main/java/GitDemoClass.java
@@ -3,7 +3,7 @@ public class GitDemoClass {
 	public static void main( String[] args ) {
 		System.out.println ("this is test by lead");
 		System.out.println ("kijal");
-		System.out.println ("retest");
+		System.out.println ("retest1");
 
 	}
 }
